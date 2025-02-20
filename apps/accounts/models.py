61133df808d7819307f1a239d0bdfde503a4905e
Baiel <jyrgalbekuulubaiel@gmail.com>
2025-02-20 17:07:59 +0700

    configured postgresdb, created manager for user authentification, created base user model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.core.validators import RegexValidator
 from django.db import models
+from django.db.models.lookups import Regex
+from django.utils import timezone
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    created = models.DateTimeField(default=timezone.now)
+    phone_regex = RegexValidator(regex=r'^((\+7)|8)\d{10}$',
+    message="Номер телефона должен быть в формате: '+79999999999' или '89999999999'. Допускается до 12 цифр.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=12, null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module(self,app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
